380bd93733cb4ee6aaa3619abcb076cbcd8781e2
holgermueller <9jholger@gmail.com>
2023-11-18 19:03:36 -0600

    added arrays defined functions

---
+++ index.js
@@ -0,0 +1,37 @@
+"use strict";
+
+let generatedName = "";
+let consonants = [
+  "b",
+  "c",
+  "d",
+  "f",
+  "g",
+  "h",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+let vowels = ["a", "e", "i", "o", "u"];
+let numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+let dash = "-";
+
+const chooseConsonant = () => {};
+
+const chooseVowel = () => {};
+
+const chooseNumber = () => {};
+
+const addDash = () => {};
