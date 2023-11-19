4c7dbb62f7d558d10b603e0881ad8c9d51370b9c
holgermueller <9jholger@gmail.com>
2023-11-19 14:29:29 -0600

    added modal

--- index.js
+++ index.js
@@ -74,3 +74,17 @@ const clearWord = () => {
 const pushNameToDOM = () => {
   document.getElementById("nameHolder").innerHTML = generatedName;
 };
+
+const modal = document.getElementById("myModal");
+
+const button = document.getElementById("modalButton");
+
+const closeButton = document.getElementById("span");
+
+const showModal = () => {
+  modal.style.display = "block";
+};
+
+const hideModal = () => {
+  modal.style.display = "none";
+};
